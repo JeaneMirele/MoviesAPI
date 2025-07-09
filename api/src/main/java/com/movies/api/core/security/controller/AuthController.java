dad13e614f59e4604c813a78f17641c912e77225
JeaneMirele <jeanemirele7@gmail.com>
2025-07-09 01:22:42 -0300

    jwt e security

---
+++ api/src/main/java/com/movies/api/core/security/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.movies.api.core.security.controller;
+
+import com.movies.api.core.security.dto.LoginDto;
+import com.movies.api.core.security.service.TokenService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/token")
+    public String getToken(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.username(), loginDto.password())
+        );
+        return tokenService.generateToken(authentication);
+        }
+}
